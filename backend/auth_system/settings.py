56080e4d48bcbc872d63424a49ea2214ac5aad8e
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-11-16 17:34:23 +0330

    email and djoser configuration, part 2

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -145,3 +145,32 @@ EMAIL_HOST_USER = 'mohsen.ansari.bim@gmail.com'
 # this below is the app password
 EMAIL_HOST_PASSWORD = 'xofb dgtk ftyu dcne'
 EMAIL_USE_TLS = True
+
+# Read about these in djoser.readthedocs.io
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE': True,
+    'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
+    'SEND_CONFIRMATION_EMAIL': True,
+    'SET_USERNAME_RETYPE': True,
+    'SET_PASSWORD_RETYPE': True,
+    'PASSWORD_RESET_CONFIRM_URL': 'password-reset/confirm/{uid},{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'username-reset/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
+    'SERIALIZERS': {
+
+    }
+
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
