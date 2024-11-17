b3c588d8f6508190366a1b0f12276eb8152873c0
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-11-17 17:18:30 +0330

    changed back to normal

--- backend/auth_system/urls.py
+++ backend/auth_system/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
          name='api-docs'),
 ]
 
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
